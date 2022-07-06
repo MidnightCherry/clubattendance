462e8961a07e862084b47d6904f54c9c631ac87c
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-07 00:08:49 +0800

    Change error message

--- public/attendee/activitiesList.php
+++ public/attendee/activitiesList.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (!isset($_SESSION["attendee_id"])){
-        $_SESSION["userErrCode"] = "STUDENT_ID_NOT_SET";
+        $_SESSION["userErrCode"] = "ATTENDEE_ID_NOT_SET";
         $_SESSION["userErrMsg"] = "The session has expired or is invalid. Please login again. Do contact the administrator if you believe that this should not happen.";
         header("refresh:0;url=/login.php?error=true");
         die();
