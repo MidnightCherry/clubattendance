6378b031c83216e95e9061b1dc038156d0f3cc50
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-07 01:06:05 +0800

    Test Code

--- public/api/getActivitiesList.php
+++ public/api/getActivitiesList.php
@@ -4,11 +4,10 @@
     class Activities {
         public $conn;
         public $outputArray;
-        function __construct($dbConnection){
-            date_default_timezone_set("Asia/Kuala_Lumpur");
-            $dateNow = date('Y-m-d');
-            $timeNow = date('H:i:s');
+        public $currDate;
+        function __construct($dbConnection, $currentDate){
             $this->conn = $dbConnection;
+            $this->currDate = $currentDate;
 
             $trackingSql = "SELECT a.app_name, c.club_name, a.app_startDate, a.app_endDate, a.app_time FROM applications AS a JOIN students AS s ON s.student_id = a.student_id JOIN clubs AS c ON c.club_id = s.club_id WHERE approved = 1";
             $res = mysqli_query($this->conn, $trackingSql);
@@ -23,7 +22,7 @@
                     array_push($columnArray, $currRowColumn[2]);
                     array_push($columnArray, $currRowColumn[3]);
                     array_push($columnArray, $currRowColumn[4]);
-                    if($currRowColumn[3] > $dateNow){
+                    if($currRowColumn[3] > $this->currDate){
                         array_push($columnArray, '<button class="d-grid mx-auto btn btn-danger" style="display: block;" id="viewAppButton">Attendance Closed</button>');
                     } else {
                         array_push($columnArray, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton">Fill Attendance</button>');
@@ -45,8 +44,11 @@
     }
     
     if($_SERVER["REQUEST_METHOD"] == "GET"){
+        date_default_timezone_set("Asia/Kuala_Lumpur");
+        $dateNow = date('Y-m-d');
+        $timeNow = date('H:i:s');
         header("Content-Type: application/json");
-        $atvt = new Activities($conn);
+        $atvt = new Activities($conn, $dateNow);
         echo $atvt->getActivitiesJson();
     }
 ?>
