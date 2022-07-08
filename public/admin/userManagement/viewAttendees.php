ae397676ec46e2bd1817df79688922f6622c420f
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-09 00:02:27 +0800

    Change button color

--- public/admin/userManagement/viewAttendees.php
+++ public/admin/userManagement/viewAttendees.php
@@ -72,7 +72,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary" onclick="window.location.href = 'deleteUser.php?user_id='+dataFirstColumn;">Proceed</button>
+                <button type="button" class="btn btn-danger" onclick="window.location.href = 'deleteUser.php?user_id='+dataFirstColumn;">Proceed</button>
             </div>
             </div>
         </div>
