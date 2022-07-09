e61112b06620e72da12036bd2ec40d94471b77b1
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-09 16:27:40 +0800

    Edit modal text

--- public/admin/userManagement/viewAttendees.php
+++ public/admin/userManagement/viewAttendees.php
@@ -68,8 +68,11 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <div class="alert alert-danger">
+                    <b>WARNING: THIS ACTION IS IRREVERSIBLE!</b>
+                </div>
                 <p>Do you really want to delete this user?</p>
-                <b>THIS ACTION IS IRREVERSIBLE!</b>
+                <b class="text-danger">Deleting this user will also delete the associated attendances made by the user.</b>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
