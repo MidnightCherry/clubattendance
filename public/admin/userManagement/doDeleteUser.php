ed01b98406e3b03485839dd51da6cacac7dbd42d
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-09 16:28:14 +0800

    Delete from users table because cascade is set

--- public/admin/userManagement/doDeleteUser.php
+++ public/admin/userManagement/doDeleteUser.php
@@ -26,43 +26,6 @@
     $role = $_GET["role"];
     if($_SERVER("REQUEST_METHOD") == "GET"){
         //what to do?
-        //delete from role tables
-        switch($role){
-            case "0":
-                //student
-                $delRoleSql = "DELETE FROM students WHERE user_id = $userId";
-                break;
-            case "1":
-                //admin
-                $delRoleSql = "DELETE FROM admins WHERE user_id = $userId";
-                break;
-            case "2":
-                //officer
-                $delRoleSql = "DELETE FROM officers WHERE user_id = $userId";
-                break;
-            case "3":
-                //attendee
-                $delRoleSql = "DELETE FROM attendees WHERE user_id = $userId";
-                break;
-            default:
-                break;
-        }
-        $delRes = mysqli_query($conn, $delRoleSql);
-        if(is_bool($delRes)){
-            if($delRes){
-                //success update
-            } else {
-                $_SESSION["userErrCode"] = "MYSQL_ERROR";
-                $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn).". Please contact the administrator if you believe that this should not happen.";
-                header("refresh:0;url=$backPage?error=true");
-                die();
-            }
-        } else {
-            $_SESSION["userErrCode"] = "MYSQL_ERROR";
-            $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-            header("refresh:0;url=$backPage?error=true");
-            die();
-        }
         //delete from users table
         $delUserSql = "DELETE FROM users WHERE user_id = $userId";
         $delRes = mysqli_query($conn, $delUserSql);
