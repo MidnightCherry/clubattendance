f42a297611820c84f6c0dc6b8818371b1b5af94d
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-12 01:49:15 +0800

    Test code

--- public/admin/userManagement/editUser.php
+++ public/admin/userManagement/editUser.php
@@ -212,7 +212,13 @@
             xmlhttp.send();
             $(document).ready(function() {
                 $('#updateForm').on('input change', function() {
-                    if(($('#name').val() != "<?php echo $thisApp[0] ?>") || ($('#email').val() != "<?php echo $thisApp[1] ?>") || ($('#telephone').val() != "<?php echo $thisApp[2] ?>") || ($('#password').val().length >= 1) <?php if($userType == 0){echo "|| ($('#clublist).val() == ".$thisApp[3].")";} else if($userType == 1){echo "|| ($('#courseCode').val() == ".$thisApp[3].")";} ?>){
+                    if(($('#name').val() != "<?php echo $thisApp[0] ?>") || ($('#email').val() != "<?php echo $thisApp[1] ?>") || ($('#telephone').val() != "<?php echo $thisApp[2] ?>") || ($('#password').val().length >= 1)<?php
+                        if($userType == 0){
+                            echo "|| ($('#clublist).val() == ".$thisApp[3].")";
+                        } else if($userType == 3){
+                            echo "|| ($('#courseCode').val() == ".$thisApp[3].")";
+                        }
+                    ?>){
                         $('#submitButton').attr('disabled', false);
                     } else {
                         $('#submitButton').attr('disabled', true);
