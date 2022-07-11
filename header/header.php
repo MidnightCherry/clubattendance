5d19107203bd6663f3bbe4f49aa3a9aec131416c
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-12 00:16:47 +0800

    Design

--- header/header.php
+++ header/header.php
@@ -113,7 +113,7 @@
         <div class="modal-content bg-uitm">
             <div class="modal-header">
                 <h5 class="modal-title" id="loginModalLabel">Sign In</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <?php 
